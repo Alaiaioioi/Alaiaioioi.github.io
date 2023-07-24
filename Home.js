9ae7445b48e32a61138e5bf6923c239f88128a8b
Alaiaioioi <121747111+Alaiaioioi@users.noreply.github.com>
2023-07-24 02:16:54 +0100

    Update Home.js

--- Home.js
+++ Home.js
@@ -41,7 +41,7 @@ let meshes = [];  // 用来保存模型的所有Mesh的数组
 
 // Create a texture loader
 const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('./plesiosaur/textures/material_0_baseColor.jpeg', function (texture) {
+const texture = textureLoader.load('./material_0_baseColor.jpeg', function (texture) {
   texture.wrapS = THREE.RepeatWrapping;
   texture.wrapT = THREE.RepeatWrapping;
 });
@@ -49,7 +49,7 @@ const texture = textureLoader.load('./plesiosaur/textures/material_0_baseColor.j
 // Create a GLTF loader
 const loader = new THREE.GLTFLoader();
 loader.load(
-  "./plesiosaur/scene.gltf",
+  "./scene.gltf",
 
   // onLoad callback
   function (gltf) {
